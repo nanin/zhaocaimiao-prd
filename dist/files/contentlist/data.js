9ff5bde1e5735fab6aeac721030bf9fbc1c744e6
马杰星 <jiexing.ma@gmail.com>
2017-02-13 14:10:18 +0800

    ini

---
+++ dist/files/contentlist/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,[j,k,l,m,n,o,p,q,r,s,t,u],v,_(w,x,y,z,A,B,C,_(),D,_(E,F,G,H,I,_(J,K,L,M),N,null,O,H,P,H,Q,R,S,null,T,U,V,W,X,Y,Z,U),ba,_(),bb,_(),bc,_(bd,[])),be,_(),bf,_());}; 
+var b="url",c="contentlist.html",d="generationDate",e=new Date(1484204370296.08),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="variables",j="OnLoadVariable",k="NewVariable",l="Height",m="NumberSelected",n="TotalItems",o="ID",p="New",q="ReTransContent",r="NumCand",s="TempVariable",t="TotalCounts",u="IsSelectedCounts",v="page",w="packageId",x="8f70a8b292414d1aafef0d5a3d072a9d",y="type",z="Axure:Page",A="name",B="contentList",C="notes",D="style",E="baseStyle",F="627587b6038d43cca051c114ac41ad32",G="pageAlignment",H="near",I="fill",J="fillType",K="solid",L="color",M=0xFFFFFFFF,N="image",O="imageHorizontalAlignment",P="imageVerticalAlignment",Q="imageRepeat",R="auto",S="favicon",T="sketchFactor",U="0",V="colorStyle",W="appliedColor",X="fontName",Y="Applied Font",Z="borderWidth",ba="adaptiveStyles",bb="interactionMap",bc="diagram",bd="objects",be="masters",bf="objectPaths";
+return _creator();
+})());
---
+++ dist/files/面试进展/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,[j,k,l,m,n,o,p,q,r,s,t,u],v,_(w,x,y,z,A,B,C,_(),D,_(E,F,G,H,I,_(J,K,L,M),N,null,O,H,P,H,Q,R,S,null,T,U,V,W,X,Y,Z,U),ba,_(),bb,_(),bc,_(bd,[])),be,_(),bf,_());}; 
+var b="url",c="面试进展.html",d="generationDate",e=new Date(1484204373298.87),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="variables",j="OnLoadVariable",k="NewVariable",l="Height",m="NumberSelected",n="TotalItems",o="ID",p="New",q="ReTransContent",r="NumCand",s="TempVariable",t="TotalCounts",u="IsSelectedCounts",v="page",w="packageId",x="949db1ff734b4a1f9e42d8e39fdd51a3",y="type",z="Axure:Page",A="name",B="面试进展",C="notes",D="style",E="baseStyle",F="627587b6038d43cca051c114ac41ad32",G="pageAlignment",H="near",I="fill",J="fillType",K="solid",L="color",M=0xFFFFFFFF,N="image",O="imageHorizontalAlignment",P="imageVerticalAlignment",Q="imageRepeat",R="auto",S="favicon",T="sketchFactor",U="0",V="colorStyle",W="appliedColor",X="fontName",Y="Applied Font",Z="borderWidth",ba="adaptiveStyles",bb="interactionMap",bc="diagram",bd="objects",be="masters",bf="objectPaths";
+return _creator();
+})());
