b44b582775e84798e70c531b4ac9bc58df7084fa
kwgjjeffrey <yuzhyuan@gmail.com>
2017-02-28 14:57:58 +0800

    no message

--- Jeffrey/files/人才库ui还原问题/data.js
+++
@@ -1,7 +0,0 @@
-﻿$axure.loadCurrentPage(
-(function() {
-    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
-    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,[j,k,l,m,n,o,p,q,r],s,_(t,u,v,w,x,y,z,_(),A,_(B,C,D,E,F,_(G,H,I,J),K,null,L,E,M,E,N,O,P,null,Q,R,S,T,U,V,W,R),X,_(),Y,_(),Z,_(ba,[])),bb,_(),bc,_());}; 
-var b="url",c="人才库ui还原问题.html",d="generationDate",e=new Date(1488264634174.35),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="variables",j="OnLoadVariable",k="item1",l="item2",m="item3",n="NewVariable2",o="NewVariable3",p="NewVariable4",q="NewVariable5",r="NewVariable6",s="page",t="packageId",u="74e46b4017d14809b6b51ea063a6f665",v="type",w="Axure:Page",x="name",y="人才库UI还原问题",z="notes",A="style",B="baseStyle",C="627587b6038d43cca051c114ac41ad32",D="pageAlignment",E="near",F="fill",G="fillType",H="solid",I="color",J=0xFFFFFFFF,K="image",L="imageHorizontalAlignment",M="imageVerticalAlignment",N="imageRepeat",O="auto",P="favicon",Q="sketchFactor",R="0",S="colorStyle",T="appliedColor",U="fontName",V="Applied Font",W="borderWidth",X="adaptiveStyles",Y="interactionMap",Z="diagram",ba="objects",bb="masters",bc="objectPaths";
-return _creator();
-})());
